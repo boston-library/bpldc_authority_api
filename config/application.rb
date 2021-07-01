8de708edb33185dc2d020b06947507279e8ad74d
bbarberBPL <bbarber@bpl.org>
2021-07-01 12:34:22 -0400

    Updated dependencies
    Updated Dockerfile
    
    Removed pry-rails and added console initialization block in application.rb

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,15 @@ module BpldcAuthorityApi
       g.factory_bot dir: 'spec/factories'
     end
 
+    if Rails.env.development?
+      console do
+        require 'pry' unless defined? Pry
+        require 'awesome_print'
+        AwesomePrint.pry!
+        config.console = Pry
+      end
+    end
+
     # NOTE Geomash doesn't read ERB/ENV in config/geomash.yml, so we reset it here
     Geomash.config[:geonames_username] = ENV.fetch('GEONAMES_USERNAME') { Rails.application.credentials.dig(:geonames_user) }
     Geomash.config[:google_key] = ENV.fetch('GOOGLE_MAPS_API_KEY') { Rails.application.credentials.dig(:google_maps_api_key) }
