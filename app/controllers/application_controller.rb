604b13bde200798c02e22d5f2b2d657ff51a964e
miao-bpl <rmiao@bpl.org>
2022-04-25 16:09:02 -0400

    Update user for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
 
   APP_INFO = {
     app_name: 'bpldc_authority_api',
-    organization: 'Rhode Island Public Library',
+    organization: 'Boston Public Library',
     version: '1.0',
   }.freeze
 
