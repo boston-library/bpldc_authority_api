2d7f0ed8a1257c3fc18301795f2f267a7570ae9d
miao-bpl <rmiao@bpl.org>
2022-11-23 11:06:07 -0500

    Use credentials for deployment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,6 +10,7 @@ set :branch, 'capistrano'
 
 # role-based syntax
 # ==================
+
 # Defines a role with one or multiple servers. The primary server in each
 # group is considered to be the first unless any hosts have the primary
 # property set. Specify the username and a domain or IP for the server.
