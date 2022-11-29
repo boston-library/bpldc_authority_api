650e67ed5b923185631ff6139c4cc027916a30b1
daniel <rbmiao@gmail.com>
2022-11-29 15:01:33 -0500

    Testing capistrano

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
 
   APP_INFO = {
     app_name: 'bpldc_authority_api',
-    organization: 'Boston Public Library',
+    organization: 'Tesla Public Library',
     version: '1.0',
   }.freeze
 
