daf27130f039ebf78b8d645ddeaeb796898042f9
miao-bpl <rmiao@bpl.org>
2022-11-30 16:28:05 -0500

    Clean up

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,63 +1,32 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-# set :application, "my_app_name"
-# set :repo_url, "git@example.com:me/my_repo.git"
+
 set :application, "bpldc_authority_api"
 set :repo_url, "https://github.com/boston-library/bpldc_authority_api.git"
 set :rails_env, 'staging'
 set :rvm_ruby_version, File.read(File.expand_path('./../.ruby-version', __dir__)).strip
 
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
 set :deploy_to, "/home/manager/bpldc_authority_api"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
 set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
-# append :linked_files, "config/database.yml", "config/staging.key", 
-
 
 ## When running tasks against staging server, some tasks defined in it needs to be available.
 ## config/deploy/staging.rb cannot be removed from <project>/shared/ directory, because it is temporarily not forcibly using ssl.
 ## Otherwise curl 172.29.101.160 returns 301....
 append :linked_files, "config/database.yml", "config/staging.key", "config/credentials/staging.key", "config/environments/staging.rb"
-# append :linked_files, "config/database.yml", "config/staging.key", "config/credentials/staging.key"
-
-## , "lib/capistrano/tasks/bpldc_nginx.rake"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "public/packs", "node_modules", "bundle", "vendor", "storage"
 
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :default_env, { node_env: :staging }
-
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+# Costomized tasks that restart our services
 namespace :boston_library do
     desc "Greeting from BOSTON PUBLIC LIBRARY"
     task :hello do
@@ -91,17 +60,8 @@ namespace :boston_library do
 	    end
 	  end
 
-  #  desc "Send config/deploy/staging.rb(with latest ruby version) to shared/config/environments/staging.rb on target server"
-  #  task :upload_staging_rb do
-  #    on roles(:app) do
-  #      upload!('./config/deploy/staging.rb', '/tmp/staging.rb')
-  #      # upload!('./config/deploy/staging.rb', '/home/manager/bpldc_authority_api/shared/config/environments/staging.rb')
-  #    end
-  #  end
-
 end
 
-# before :'deploy:set_current_revision', :'boston_library:upload_staging_rb'
 before :'bundler:install', :'boston_library:gem_update'
 before :'deploy:migrate', :'boston_library:hello'
 after 'deploy:cleanup', 'boston_library:restart_bpldc_nginx'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,19 +1,9 @@
 # server-based syntax
 # ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
-
-
-## set :rvm_ruby_version, '3.0.4'
+# Grab ruby version from project home directory. It is needed for deployment.
 
 set :rvm_ruby_version, File.read(File.expand_path('./../../.ruby-version', __dir__)).strip
-
-## server "####", user: "######", password: "#{####}", roles: %w{app db}
-## server ENV.fetch('BPLDC_AUTHORITY_API_DEPLOY_SERVER'){ Rails.application.credentials.dig(:deploy, :server) }, user: ENV.fetch('BPLDC_AUTHORITY_API_DEPLOY_USER'){ Rails.application.credentials.dig(:deploy, :user) }, password: ENV.fetch('BPLDC_AUTHORITY_API_DEPLOY_PASSWORD'){ Rails.application.credentials.dig(:deploy, :password) }, roles: %w{app db}
 
 # set :branch, 'jenkins'
 set :branch, 'capistrano'
@@ -26,56 +16,18 @@ set :branch, 'capistrano'
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
 role :app, %w{manager@172.29.101.160}
 role :web, %w{manager@172.29.101.160}
 role :db,  %w{manager@172.29.101.160}
 
-# before :'bundler:install', :'boston_library:gem_update'
-# before :'deploy:migrate', :'boston_library:hello'
 
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
+## When Capistrano tries to delete old release, puma socket/id can be removed only by sudo user.
+## Allow current user to run it with sudo priviledge.
 SSHKit.config.command_map[:rm] = 'sudo rm'
 
 # Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
-
+# SSH to remote server uses username/password.
+# For security reason, here uses ssh key.
 server '172.29.101.160', {
   :user => 'manager',
   :role => %w{app db web},
