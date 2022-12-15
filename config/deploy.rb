8bb47e5b4a175f2de494f3b01fa5db9bd468b1bc
miao-bpl <rmiao@bpl.org>
2022-12-15 16:37:48 -0500

    Change variable names from rvm_ruby_version to ruby_version, rvm_bundle_verion to bundle_version

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,8 +13,8 @@ set :user, Rails.application.credentials.dig(:deploy, :user)
 set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 
 set :rvm_installed, "/home/#{fetch(:user)}/.rvm/bin/rvm"
-set :rvm_ruby_version, File.read(File.expand_path('./../.ruby-version', __dir__)).strip
-set :rvm_bundle_version, File.read(File.expand_path('./Gemfile.lock'))[-10..-1].strip
+set :ruby_version, File.read(File.expand_path('./../.ruby-version', __dir__)).strip
+set :bundle_version, File.read(File.expand_path('./Gemfile.lock'))[-10..-1].strip
 ## Gemfile.lock show puma version as "    puma (5.6.5) " -  don't remove space from "/ /"
 set :rvm_puma_version, File.readlines('./Gemfile.lock').reverse.find { |v| v =~ /    puma/ }[-7..-3]
 
@@ -35,15 +35,15 @@ namespace :boston_library do
   desc 'Gem update'
   task :gem_update do
     on roles(:app) do
-      execute("#{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do gem update --system --no-document")
+      execute("#{fetch(:rvm_installed)} #{fetch(:ruby_version)} do gem update --system --no-document")
     end
   end
 
   desc 'Install new ruby if ruby-version is required'
   task :rvm_install_ruby do
     on roles(:app) do
-      execute("#{fetch(:rvm_installed)} install #{fetch(:rvm_ruby_version)} -C --with-jemalloc")
-      execute("#{fetch(:rvm_installed)} use #{fetch(:rvm_ruby_version)}")
+      execute("#{fetch(:rvm_installed)} install #{fetch(:ruby_version)} -C --with-jemalloc")
+      execute("#{fetch(:rvm_installed)} use #{fetch(:ruby_version)}")
     end
   end
 
@@ -51,8 +51,8 @@ namespace :boston_library do
   desc "Install bundler #{fetch(:rvm_bundle_version)}"
   task :install_bundler do
     on roles(:app) do
-      execute("#{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do gem install bundler:#{fetch(:rvm_bundle_version)}")
-      execute("#{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do gem install puma:#{fetch(:rvm_puma_version)}")
+      execute("#{fetch(:rvm_installed)} #{fetch(:ruby_version)} do gem install bundler:#{fetch(:bundle_version)}")
+      execute("#{fetch(:rvm_installed)} #{fetch(:ruby_version)} do gem install puma:#{fetch(:puma_version)}")
     end
   end
 
