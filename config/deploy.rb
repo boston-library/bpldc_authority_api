7ba97985938928cdaaad12c898a08c2adda12d6a
miao-bpl <rmiao@bpl.org>
2022-12-16 11:21:02 -0500

    Install bundler using binstubs

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ namespace :boston_library do
   desc "Install bundler #{fetch(:rvm_bundle_version)}"
   task :install_bundler do
     on roles(:app) do
-      # execute("#{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do gem install bundler:#{fetch(:bundle_version)}")
+      execute("#{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do gem install bundler:#{fetch(:bundle_version)}")
       execute("#{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do bundle binstubs bundler --force")
       # execute("#{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do gem install puma:#{fetch(:puma_version)}")
     end
