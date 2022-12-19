e86bdd67e2c8cc134ee6e69a0dd539d9242926f0
miao-bpl <rmiao@bpl.org>
2022-12-19 13:14:37 -0500

    Change variable names to exp_ruby_version, expe_bundle_version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
 
   APP_INFO = {
     app_name: 'bpldc_authority_api',
-    organization: 'Brighton Public Library',
+    organization: 'West Brighton Public Library',
     version: '1.0',
   }.freeze
 
