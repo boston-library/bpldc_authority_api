9048db0a07ab322c79ec921a3776e5121dc512a5
daniel <rbmiao@gmail.com>
2022-12-20 14:08:01 -0500

    Running puma without binstubs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
 
   APP_INFO = {
     app_name: 'bpldc_authority_api',
-    organization: 'Framingham Public Library',
+    organization: 'Lexington Public Library',
     version: '1.0',
   }.freeze
 
