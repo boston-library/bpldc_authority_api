7b6177c5e387bc83fa410a57eceb43520a68a8e6
miao-bpl <rmiao@bpl.org>
2023-01-13 14:24:03 -0500

    back to testing server for puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -34,10 +34,10 @@ on_worker_boot do
 end
 
 if %w(staging production).member?(rails_env)
-  bind "unix://#{app_dir}/tmp/sockets/bpldc_authority_api_puma.sock"
+  bind "unix://#{app_dir}/tmp/sockets/bpldc_puma.sock"
   stdout_redirect("#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true)
-  pidfile "#{app_dir}/tmp/pids/bpldc_authority_api_puma_server.pid"
-  state_path "#{app_dir}/tmp/pids/bpldc_authority_api_puma_server.state"
+  pidfile "#{app_dir}/tmp/pids/bpldc_puma_server.pid"
+  state_path "#{app_dir}/tmp/pids/bpldc_puma_server.state"
 else
   port ENV.fetch('PORT') { 3000 }
   stdout_redirect('/dev/stdout', '/dev/stderr')
