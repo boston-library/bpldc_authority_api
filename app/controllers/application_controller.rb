e77aedf3313e60169ce6331267fec3e2122c84ac
miao-bpl <rmiao@bpl.org>
2023-02-13 16:04:57 -0500

    test capistrano

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
 
   APP_INFO = {
     app_name: 'bpldc_authority_api',
-    organization: 'Utah Public Library',
+    organization: 'West Virginia Public Library',
     version: '1.0',
   }.freeze
 
