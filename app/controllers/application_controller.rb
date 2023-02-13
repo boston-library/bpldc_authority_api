e7cbe8f0b459156baf7d11bb5f309ffbc9875a2d
miao-bpl <rmiao@bpl.org>
2023-02-13 14:44:40 -0500

    test capistrano

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
 
   APP_INFO = {
     app_name: 'bpldc_authority_api',
-    organization: 'New York Public Library',
+    organization: 'Florida Public Library',
     version: '1.0',
   }.freeze
 
