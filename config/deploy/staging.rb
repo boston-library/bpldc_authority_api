bb854888d1989e0d7e4eb62a465ab0b36a485f55
miao-bpl <rmiao@bpl.org>
2024-09-04 16:12:21 -0400

    ssh key

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -29,9 +29,9 @@ server fetch(:server_ip).to_s, {
   :role => %w(app db web)
   # :role => %w(app db web),
   # :ssh_options => {
-    #    :keys => fetch(:ssh_key).to_s,
-    # m# :verbose => :debug
-    #    :forward_agent => true,
-    #    :auth_methods => %w(publickey)
+  #    :keys => fetch(:ssh_key).to_s,
+  # m# :verbose => :debug
+  #    :forward_agent => true,
+  #    :auth_methods => %w(publickey)
   # }
 }
