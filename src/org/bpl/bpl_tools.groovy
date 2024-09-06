9c37e09c21526de38a3f4a04ebd904593d9a5d77
miao-bpl <rmiao@bpl.org>
2024-09-06 14:28:32 -0400

    ssh key

--- src/org/bpl/bpl_tools.groovy
+++ src/org/bpl/bpl_tools.groovy
@@ -86,11 +86,11 @@ def RunPreparation(){
 
     set -e
 
-    /var/lib/jenkins/.rvm/bin/rvm install ${RUBYVERSION}
+    /var/lib/jenkins/.rvm/bin/rvm install ${EXPECTED_RUBY}
     ## /var/lib/jenkins/.rvm/bin/rvm get stable
-    /var/lib/jenkins/.rvm/bin/rvm use ${RUBYVERSION} --default
-    /var/lib/jenkins/.rvm/bin/rvm alias create --default  ${RUBYVERSION} 
-    /var/lib/jenkins/.rvm/bin/rvm alias create --current  ${RUBYVERSION} 
+    /var/lib/jenkins/.rvm/bin/rvm use ${EXPECTED_RUBY} --default
+    /var/lib/jenkins/.rvm/bin/rvm alias create --default  ${EXPECTED_RUBY} 
+    /var/lib/jenkins/.rvm/bin/rvm alias create --current  ${EXPECTED_RUBY} 
 
     ruby --version
 
