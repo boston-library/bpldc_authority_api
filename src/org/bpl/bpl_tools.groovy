9bd614e5479e8d593799bbc0614a4331c7a23a04
miao-bpl <rmiao@bpl.org>
2024-09-06 12:06:43 -0400

    ssh key

--- src/org/bpl/bpl_tools.groovy
+++ src/org/bpl/bpl_tools.groovy
@@ -143,6 +143,12 @@ def RunDBpreparation(railsEnv){
       else 
          exit
       fi    
+
+      /var/lib/jenkins/.rvm/bin/rvm install ${RUBYVERSION}
+         ## /var/lib/jenkins/.rvm/bin/rvm get stable
+      /var/lib/jenkins/.rvm/bin/rvm use ${RUBYVERSION} --default
+      /var/lib/jenkins/.rvm/bin/rvm alias create --default  ${RUBYVERSION} 
+      /var/lib/jenkins/.rvm/bin/rvm alias create --current  ${RUBYVERSION} 
       
       # RAILS_ENV=${RAILS_ENV} bundle exec rails db:prepare
       # RAILS_ENV=${RAILS_ENV} bundle exec rails db:migrate
