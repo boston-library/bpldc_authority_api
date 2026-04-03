71888edd0b8f1864d1bc45b8b837c92f75304877
miao-bpl <rmiao@bpl.org>
2026-04-03 12:09:18 -0400

    exclude spec/rails_helper.rb to Rails/Exit and Rails/Output

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
-raise 'The Rails environment is running in production mode!' if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 require 'rspec/rails'
 require 'database_cleaner/active_record'
@@ -59,8 +59,8 @@ end
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  Rails.logger.error(e.to_s.strip)
-  raise 'Exiting due to pending migrations.'
+  puts e.to_s.strip
+  exit 1
 end
 
 RSpec.configure do |config|
